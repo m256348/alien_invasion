620a3637daf5dc327d7f9acd24b231990c6b399b
m256348 <m256348@usna.edu>
2022-11-10 15:17:14 -0500

    Chapter 13

--- settings.py
+++ settings.py
@@ -8,12 +8,19 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (230,230,230)
 
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 100
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
+
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 3
+        self.ship_limit = 1
 
         # Bullet Settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 5
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
